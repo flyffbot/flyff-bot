8f5510dd982b0ff72e923e773ae10d80b5c17f1a
flyffbot <help.flyffbot@gmail.com>
2023-05-30 15:48:55 +0200

    bump version

--- build.bat
+++ build.bat
@@ -49,7 +49,7 @@ if defined skip_installer (
 ) else (
 	md release
 	md release\data
-	move /Y .\full-app\target\FlyffBot-0.4.4-shaded.jar .\release\data\FlyffBot.jar
+	move /Y .\full-app\target\FlyffBot-0.5.0-shaded.jar .\release\data\FlyffBot.jar
 	move /Y .\launcher\FlyffBot.exe .\release
 	call "C:\Program Files (x86)\Inno Setup 6\Compil32.exe" /cc release.iss
 )
